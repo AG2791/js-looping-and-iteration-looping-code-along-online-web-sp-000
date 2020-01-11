2ffdbf1aba996d2f5838cfdf5a2347703163a352
AJ Guiles <ashiguiles@gmail.com>
2020-01-11 07:11:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+let cards =[]
+
+function writeCards(names,event) {
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  
+  return cards
+}
+
+
+function countdown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
+  }
+  console.log( countdown );
+}
